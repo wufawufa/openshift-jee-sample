896d69b7943e2f242ded1de52b5e6139901d2a35
George Yang <george.yang@infarmbureau.com>
2019-02-04 22:30:33 -0500

    Make the index a smaller page for easy change during demo

---
+++ src/main/java/com/mysample/scratch.java
@@ -0,0 +1,18 @@
+package com.mysample;
+
+import java.util.HashMap;
+import javax.enterprise.context.RequestScoped; import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+
+@RequestScoped @Path("/support")
+public class SupportResource {
+    @GET()
+    @Produces("application/json")
+    public HashMap<String,String> getSupport() {
+        HashMap<String, String> theSupport = new HashMap<String, String>(); theInsult.put("support this week", "Senthil"); return theSupport;
+
+        
+    }
+}
